491f0daf6dedbcd8ff437d3bee464be934ca03c0
dkt64 <b.apanasewicz@gmail.com>
2019-09-19 15:19:08 +0200

    first working version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to plotly.js DTP-VISU app</h1>
+      </v-flex>
+    </v-layout>
+
+    <v-layout text-center wrap>
+      <v-flex mb-4>
+        <Plotly :data="tab" :layout="layout" :display-mode-bar="false"></Plotly>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { Plotly } from "vue-plotly";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+var z1 = [
+  [6.7, 6.9, 7, 7, 7, 7, 6.9, 6.7],
+  [2, 2, 2, 2, 2, 2, 2, 2],
+  [6.7, 6.9, 7, 7, 7, 7, 6.9, 6.7]
+  // [8.83, 8.89, 8.81, 8.87, 8.9, 8.87],
+  // [8.89, 8.94, 8.85, 8.94, 8.96, 8.92],
+  // [8.84, 8.9, 8.82, 8.92, 8.93, 8.91],
+  // [8.79, 8.85, 8.79, 8.9, 8.94, 8.92],
+  // [8.79, 8.88, 8.81, 8.9, 8.95, 8.92],
+  // [8.8, 8.82, 8.78, 8.91, 8.94, 8.92],
+  // [8.75, 8.78, 8.77, 8.91, 8.95, 8.92],
+  // [8.8, 8.8, 8.77, 8.91, 8.95, 8.94],
+  // [8.74, 8.81, 8.76, 8.93, 8.98, 8.99],
+  // [8.89, 8.99, 8.92, 9.1, 9.13, 9.11],
+  // [8.97, 8.97, 8.91, 9.09, 9.11, 9.11],
+  // [9.04, 9.08, 9.05, 9.25, 9.28, 9.27],
+  // [9, 9.01, 9, 9.2, 9.23, 9.2],
+  // [8.99, 8.99, 8.98, 9.18, 9.2, 9.19],
+  // [8.93, 8.97, 8.97, 9.18, 9.2, 9.18]
+];
+
+export default {
+  components: {
+    Plotly
+  },
+  data: () => ({
+    tab: [
+      {
+        y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        // y: [10, 15, 13, 17],
+        z: z1,
+        type: "surface"
+      }
+    ],
+
+    layout: {
+      title: "Wykres",
+      // scene: { camera: { eye: { x: 1.87, y: 0.88, z: -0.64 } } },
+      autosize: true,
+      // width: 800,
+      height: 700
+      // margin: {
+      //   l: 65,
+      //   r: 50,
+      //   b: 65,
+      //   t: 90
+      // }
+    }
+  }),
+  created() {
+    // eslint-disable-next-line
+    console.log("created()...");
+    // eslint-disable-next-line
+    // console.log(this.tab[0].y[0]);
+  }
+};
+</script>
